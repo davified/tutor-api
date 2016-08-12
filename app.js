2d9183a4cb402e269c8d2669727914e9b040f12a
David Tan <davified@gmail.com>
2016-08-12 09:34:03 +0800

    enabled CORS

--- app.js
+++ app.js
@@ -20,12 +20,12 @@ app.use(cors())
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, User-Email, Auth-Token')
-//   res.header('Access-Control-Allow-Methods', 'POST, PATCH, GET, OPTIONS')
-//   next()
-// })
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, User-Email, Auth-Token')
+  res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, OPTIONS')
+  next()
+})
 
 mongoose.connect(process.env.MONGODB_URI)
 
